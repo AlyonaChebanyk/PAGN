a0f7574fdac95f26b5e27e3ccbb844f62f0e24c6
Andy <martinyukaa@ukr.net>
2021-10-03 15:56:39 +0300

    Rework

--- laba2.py
+++ laba2.py
@@ -3,25 +3,34 @@ import numpy as np
 import argparse
 import sys
 
+X_ARRAY = np.arange(-2, 3, 0.1)
+Y_ARRAY = np.arange(-0.5, 2, 0.1)
+
+class_colors = {
+    '1': 'r',
+    '2': 'b',
+    '3': 'g',
+}
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--d12_A', type=float)
-parser.add_argument('--d12_B', type=float)
-parser.add_argument('--d12_C', type=float)
-parser.add_argument('--d13_D', type=float)
-parser.add_argument('--d13_E', type=float)
-parser.add_argument('--d13_F', type=float)
-parser.add_argument('--d23_G', type=float)
-parser.add_argument('--d23_H', type=float)
-parser.add_argument('--d23_K', type=float)
-parser.add_argument('--d1_M', type=float)
-parser.add_argument('--d1_N', type=float)
-parser.add_argument('--d1_P', type=float)
-parser.add_argument('--d2_Q', type=float)
-parser.add_argument('--d2_R', type=float)
-parser.add_argument('--d2_S', type=float)
-parser.add_argument('--d3_V', type=float)
-parser.add_argument('--d3_W', type=float)
-parser.add_argument('--d3_Z', type=float)
+parser.add_argument('--d12_A', type=float, default=1)
+parser.add_argument('--d12_B', type=float, default=-1)
+parser.add_argument('--d12_C', type=float, default=-0.1)
+parser.add_argument('--d13_D', type=float, default=2)
+parser.add_argument('--d13_E', type=float, default=1)
+parser.add_argument('--d13_F', type=float, default=-1.2)
+parser.add_argument('--d23_G', type=float, default=1)
+parser.add_argument('--d23_H', type=float, default=6)
+parser.add_argument('--d23_K', type=float, default=-5)
+parser.add_argument('--d1_M', type=float, default=-1)
+parser.add_argument('--d1_N', type=float, default=1)
+parser.add_argument('--d1_P', type=float, default=-0.3)
+parser.add_argument('--d2_Q', type=float, default=1)
+parser.add_argument('--d2_R', type=float, default=1)
+parser.add_argument('--d2_S', type=float, default=-1.2)
+parser.add_argument('--d3_V', type=float, default=-1)
+parser.add_argument('--d3_W', type=float, default=-3)
+parser.add_argument('--d3_Z', type=float, default=1.7)
 
 namespace = parser.parse_args(sys.argv[1:])
 
@@ -46,8 +55,9 @@ D3_Z = namespace.d3_Z
 
 print(namespace)
 
+
 def find_cos(x1, y1, x2, y2):
-    return (x1*x2 + y1*y2) / ( (np.sqrt(x1**2+y1**2)) * np.sqrt(x2**2+y2**2))
+    return (x1 * x2 + y1 * y2) / ((np.sqrt(x1 ** 2 + y1 ** 2)) * np.sqrt(x2 ** 2 + y2 ** 2))
 
 
 def define_class(x, y):
@@ -72,43 +82,40 @@ def define_class(x, y):
 
 
 # точки для прорисовки прямых решающих функций
-x1_d12 = -0.5
+fig, ax = plt.subplots()
+ax.set_xlim([min(X_ARRAY), max(X_ARRAY)])
+ax.set_ylim([min(Y_ARRAY), max(Y_ARRAY)])
+x1_d12 = X_ARRAY[0]
 y1_d12 = (-D12_A * x1_d12 - D12_C) / D12_B
-y2_d12 = 2
-x2_d12 = (-D12_B * y2_d12 - D12_C) / D12_A
-plt.plot([x1_d12, x2_d12], [y1_d12, y2_d12], c='k')
+x2_d12 = X_ARRAY[-1]
+y2_d12 = (-D12_A * x2_d12 - D12_C) / D12_B
+ax.plot([x1_d12, x2_d12], [y1_d12, y2_d12], c='k', linestyle=':', linewidth=2.5)
 print(find_cos(x1_d12, y1_d12, x2_d12, y2_d12))
 
-x1_d13 = -0.5
+x1_d13 = X_ARRAY[0]
 y1_d13 = (-D13_D * x1_d13 - D13_F) / D13_E
-y2_d13 = -0.7
-x2_d13 = (-D13_E * y2_d13 - D13_F) / D13_D
-plt.plot([x1_d13, x2_d13], [y1_d13, y2_d13], c='k')
+x2_d13 = X_ARRAY[-1]
+y2_d13 = (-D13_D * x2_d13 - D13_F) / D13_E
+ax.plot([x1_d13, x2_d13], [y1_d13, y2_d13], c='k', linestyle='--', linewidth=2.5)
 
-x1_d23 = -2
+x1_d23 = X_ARRAY[0]
 y1_d23 = (-D23_G * x1_d23 - D23_K) / D23_H
-y2_d23 = 0.3
-x2_d23 = (-D23_H * y2_d23 - D23_K) / D23_G
-plt.plot([x1_d23, x2_d23], [y1_d23, y2_d23], c='k')
+x2_d23 = X_ARRAY[-1]
+y2_d23 = (-D23_G * x2_d23 - D23_K) / D23_H
+plt.plot([x1_d23, x2_d23], [y1_d23, y2_d23], c='k', linestyle='-.', linewidth=2.5)
 
-plt.text(1.5, 1.6, 'd12', rotation=y1_d12/x1_d12*45, fontsize='x-large')
+ax.text(1.5, 1.6, 'd12', rotation=y1_d12 / x1_d12 * 45, fontsize='x-large')
 
-x_indices = np.arange(-2, 3, 0.1)
-y_indices = np.arange(-0.5, 2, 0.1)
+X_ARRAY = np.arange(-2, 3, 0.1)
+Y_ARRAY = np.arange(-0.5, 2, 0.1)
 
-class_colors = {
-    '1': 'r',
-    '2': 'b',
-    '3': 'g',
-}
-
-for x in x_indices:
-    for y in y_indices:
+for x in X_ARRAY:
+    for y in Y_ARRAY:
         class_ = define_class(x, y)
         if class_ != 'undefined':
-            plt.scatter(x, y, c=class_colors[class_], alpha=0.4, s=8)
+            plt.scatter(x, y, c=class_colors[class_], alpha=1, s=8)
 
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.grid(True)
 plt.show()
