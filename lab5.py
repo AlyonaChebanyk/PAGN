bc62b10d6153ff07544a868a0a39ada6da906c70
Денис <denisden807@gmail.com>
2021-11-25 10:56:42 +0200

    reverse

--- lab5.py
+++ lab5.py
@@ -11,7 +11,7 @@ classA = np.array([[0.05, 0.91],
                    [0.14, 0.96],
                    [0.16, 0.9],
                    [0.07, 0.7],
-                   [0.2, 0,63]])
+                   [0.2, 0, 63]])
 
 classB = np.array([[0.49, 0.89],
                    [0.34, 0.81],
@@ -20,13 +20,51 @@ classB = np.array([[0.49, 0.89],
                    [0.52, 0.53]])
 
 
+def matrix_array(matrix):
+    maible = []
+    matrix = np.asarray(maible)
+
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print(i, j)
+            matrix_1 = matrix[i].transpose()
+            matrix_2 = matrix[j].transpose()
+
+            maible.append(np.dot(matrix_1, matrix_2))
+    return maible
+
+
+
+def matrix_array1(matrix):
+    maible = []
+    matrix = np.asarray(maible)
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix)):
+            print(i, j)
+            matrix_1 = matrix[i].transpose()
+            matrix_2 = matrix[j].transpose()
+
+            maible.append(np.dot(matrix_1, matrix_2))
+    return maible
+
+
 class_colors = {
     '1': 'r',
     '2': 'b',
-    '3': 'g',
-    '4': 'y'
+    'undefined': 'k'
 }
 
 
+def get_lambda(matrix_a):
+    pass
 
 
+print('magic')
+if __name__ == '__main__':
+    print('Run lab 5 ')
+    matrix_array(classA)
+    matrix_array(classB)
+
+print('magic2')
